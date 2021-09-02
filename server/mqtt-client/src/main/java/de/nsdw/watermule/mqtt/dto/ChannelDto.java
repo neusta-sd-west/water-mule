8d8d4b835caf9e685b674747905a2d16d7e8e7b9
Heiko Dietz <hdietz@neusta-sd-west.de>
2021-09-02 16:06:19 +0200

    UI Changes

--- server/mqtt-client/src/main/java/de/nsdw/watermule/mqtt/dto/ChannelDto.java
+++ server/mqtt-client/src/main/java/de/nsdw/watermule/mqtt/dto/ChannelDto.java
@@ -4,15 +4,27 @@
 //
 package de.nsdw.watermule.mqtt.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ChannelDto {
+    @JsonProperty("id")
     private String id;
+
+    @JsonProperty("deviceId")
     private String deviceId;
+
+    @JsonProperty("name")
     private String name;
+
+    @JsonProperty("description")
     private String description;
+
+    @JsonProperty("unit")
     private String unit;
 }
--- server/mqtt-client/src/main/java/de/nsdw/watermule/mqtt/dto/DeviceDto.java
+++ server/mqtt-client/src/main/java/de/nsdw/watermule/mqtt/dto/DeviceDto.java
@@ -6,8 +6,10 @@ package de.nsdw.watermule.mqtt.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class DeviceDto {
     private String id;
